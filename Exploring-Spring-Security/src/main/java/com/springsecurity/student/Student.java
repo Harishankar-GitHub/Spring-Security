6e135ff44b33fb99e00e11131f1d26e6087735fb
Harishankar Bhat R <rhsb1997@gmail.com>
2021-05-17 21:37:35 +0530

    Added an API

---
+++ Exploring-Spring-Security/src/main/java/com/springsecurity/student/Student.java
@@ -0,0 +1,15 @@
+package com.springsecurity.student;
+
+import lombok.Getter;
+
+@Getter
+public class Student
+{
+    private final Integer studentId;
+    private final String studentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+}
